9f07b52b0af946b6f862ca9ddd3a7be88d77ab6f
ShayneKaruna <112900215+ShayneKaruna@users.noreply.github.com>
2022-11-24 21:03:50 -0500

    added to pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation} from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -56,20 +57,11 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    
+    try { 
+      const {data} = saveBook ({
+        variables: {input:bookToSave}
+      })
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
